7483377bc63b9f72176de192c7f99c293095a688
jehian athaya <jehianbusiness@gmail.com>
2022-06-28 19:45:28 +0700

    swingjs emailjs

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,15 @@ import AVTR2 from '../../assets/avatar2.png'
 import AVTR3 from '../../assets/avatar3.png'
 import AVTR4 from '../../assets/avatar4.png'
 
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const data = [
   {
     avatar: AVTR1,
@@ -31,28 +40,31 @@ const data = [
 const Testimonials = () => {
   return (
     <section id='testimonials'>
-      <h5>
-        What people say about me
-      </h5>
-      <h2>
-        Testimonials
-      </h2>
-      <div className="container testimonials__container">
+      <h5>What People Say About Me</h5>
+      <h2>Testimonials</h2>
+      <Swiper className="container testimonials__container"
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      >
         {
           data.map(({avatar, name, review}, index) => {
             return (
-              <article key={index} className='testimonial'>
+              <SwiperSlide key={index} className='testimonial'>
                 <div className="client__avatar">
                   <img src={avatar}/>
                 </div>
                 <h5 className='client__name'>{name}</h5>
                 <small className='client__review'>{review}</small>
-              </article>
+              </SwiperSlide>
             )
           })
         }
         
-      </div>
+      </Swiper>
     </section>
   )
 }
